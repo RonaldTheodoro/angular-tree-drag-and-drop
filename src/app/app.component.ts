9d30ad7a09ca3418942ffe81c1449442f8fde6a6
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2020-05-18 10:38:01 -0300

    Add angular tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
 import { Component } from '@angular/core';
+import { ITreeState, ITreeOptions } from 'angular-tree-component';
+import { v4 } from 'uuid';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tree-drag-and-drop';
+    state: ITreeState = {
+        expandedNodeIds: {
+            1: true,
+            2: true
+        },
+        hiddenNodeIds: {},
+        activeNodeIds: {}
+    };
+
+    options: ITreeOptions = {
+        allowDrag: (node) => node.isLeaf,
+        getNodeClone: (node) => ({
+            ...node.data,
+            id: v4(),
+            name: `copy of ${node.data.name}`
+        })
+    };
+
+    nodes01 = [
+        {
+            id: 1,
+            name: 'root1',
+            children: [
+                { name: 'child1' },
+                { name: 'child2' }
+            ]
+        },
+        {
+            name: 'root2',
+            id: 2,
+            children: [
+                { name: 'child2.1', children: [] },
+                {
+                    name: 'child2.2', children: [
+                        { name: 'grandchild2.2.1' }
+                    ]
+                }
+            ]
+        },
+        { name: 'root3' },
+        { name: 'root4', children: [] },
+        { name: 'root5', children: null }
+    ];
+
+
+    nodes02 = [
+        {
+            id: 1,
+            name: 'root1',
+            children: [
+                { name: 'child1' },
+                { name: 'child2' }
+            ]
+        },
+        {
+            name: 'root2',
+            id: 2,
+            children: [
+                { name: 'child2.1', children: [] },
+                {
+                    name: 'child2.2', children: [
+                        { name: 'grandchild2.2.1' }
+                    ]
+                }
+            ]
+        },
+        { name: 'root3' },
+        { name: 'root4', children: [] },
+        { name: 'root5', children: null }
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TreeModule } from 'angular-tree-component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    TreeModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
